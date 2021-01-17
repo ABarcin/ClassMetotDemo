fb608f8966ece2c69afbfed0ff9031bd369ece16
ABarcin <74905523+ABarcin@users.noreply.github.com>
2021-01-17 15:58:38 +0300

    Simulasyon yapıldı

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Name +"  İsimli Müşteri Eklenmiştir");
+            Console.WriteLine(" ------------------------");
+        }
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id+" Numaralı Id e Sahip Müşteri Silinmiştir.");
+            Console.WriteLine(" ------------------------");
+        }
+        public void Edit(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id+" Numaralı Id e Sahip Müşteri Düzenlenmiştir.");
+            Console.WriteLine(" ------------------------");
+        }
+        public void ListMusteri(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id+" Numaralı Müşteri Bilgileri Aşağıda Listelenmiştir.");
+            Console.WriteLine(musteri.Name +" " +musteri.Surname);
+            Console.WriteLine(musteri.PhoneNumber);
+            Console.WriteLine(musteri.Adress);
+            Console.WriteLine(" ------------------------");
+        }
+        public void ListAllMusteriler(Musteri[] musteriler)
+        {
+            Console.WriteLine("Tüm Müşteri Isimleri Aşağıda Listelenmiştir.");
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Name + " " + musteri.Surname);
+                Console.WriteLine(" ------------------------");
+            }
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,36 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 0;
+            musteri1.Name = "Ahmet";
+            musteri1.Surname = "Barçın";
+            musteri1.PhoneNumber = "05370000000";
+            musteri1.Adress = "Kocaeli";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 1;
+            musteri2.Name = "Mehmet";
+            musteri2.Surname = "Barçın";
+            musteri2.PhoneNumber = "05360000000";
+            musteri2.Adress = "İstanbul";
+            Musteri musteri3 = new Musteri();
+
+            musteri3.Id = 2;
+            musteri3.Name = "Muhammed";
+            musteri3.Surname = "Barçın";
+            musteri3.PhoneNumber = "05350000000";
+            musteri3.Adress = "Bursa";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Edit(musteri2);
+            musteriManager.Delete(musteri3);
+            musteriManager.ListMusteri(musteri1);
+            musteriManager.ListAllMusteriler(musteriler);
+
         }
     }
 }
